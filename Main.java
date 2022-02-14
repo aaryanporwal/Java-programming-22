5744c4de863b0472b48f82a9136423da7284d3d3
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-02-14 12:37:58 +0530

    Q4 completed

--- Main.java
+++ Main.java
@@ -1,31 +1,17 @@
 import java.util.*;
 
-// Q3 Write a java program to compute the roots of a quadratic equation.
+// Q4 Design an EMI calculator that will calculate the EMI for any loan details.
 
-import java.math.*;
-public class Main {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter the values of a,b,c");
-    double a = in.nextDouble();
-    double b = in.nextDouble();
-    double c = in.nextDouble();
-    double root1, root2;
-    double determinant = b*b-4*a*c;
-    if (determinant > 0) {
-      root1=(-b+Math.sqrt(determinant))/(2*a);
-      root2=(-b-Math.sqrt(determinant))/(2*a);
-      System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
-    }
-    else if(determinant==0) {
-      root1=root2= -b/(2 * a);
-      System.out.format("root1 = root2 = %.2f;", root1);
-    }
-    else {
-      double real = -b/(2 * a);
-      double imaginary = Math.sqrt(-determinant)/(2 * a);
-      System.out.format("root1 = %.2f+%.2fi", real, imaginary);
-      System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
-    }
-  }
+public static void Main() {
+  Scanner scanner = new Scanner(System.in);
+  System.out.println(&quot;Enter P: &quot;);
+  double p = scanner.nextDouble();
+  System.out.println(&quot;Enter R: &quot;);
+  double r = scanner.nextDouble();
+  System.out.println(&quot;Enter N: &quot;);
+  double n = scanner.nextDouble(); 
+  r += 1;
+  double q = Math.pow(r, n);
+  double m = (p*q)/(q-1);
+  System.out.println(&quot;Value of M is: &quot; + m);
 }
