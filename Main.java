c3fe70a45bd15deca445ff5cc388e75debe67877
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-02-14 06:22:51 +0000

    Q1

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner tempInput = new Scanner(System.in);
+    int fahrenheitTemp = tempInput.nextInt();
+    int celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+    System.out.println("Temperature in \u00B0Celsius: " + celsiusTemp);
+    tempInput.close();
   }
 }
