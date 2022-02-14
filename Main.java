3ba786f44d81a8c397bc57a9523a63277d0d9b13
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-02-14 12:18:23 +0530

    q2 boilerplate

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
+
+// Q2 Write a java program to display all the factors of a given numnber. The number must be provided by user during run time.
+
 class Main {
   public static void main(String[] args) {
-    Scanner tempInput = new Scanner(System.in);
-    int fahrenheitTemp = tempInput.nextInt();
-    int celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+    Scanner numberInput = new Scanner(System.in);
+    
     System.out.println("Temperature in \u00B0Celsius: " + celsiusTemp);
-    tempInput.close();
+    numberInput.close();
   }
 }
+
